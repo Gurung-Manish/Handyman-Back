331c4a2ec6f2b8588dd27c9de15a68cfb9417105
Gurung-Manish <manishgrg496@gmail.com>
2023-06-23 20:43:05 +0100

    adding all files

---
+++ src/main/java/com/handyman/rest_handyman/RestHandymanApplication.java
@@ -0,0 +1,13 @@
+package com.handyman.rest_handyman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHandymanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHandymanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handyman/rest_handyman/RestHandymanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handyman.rest_handyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHandymanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
