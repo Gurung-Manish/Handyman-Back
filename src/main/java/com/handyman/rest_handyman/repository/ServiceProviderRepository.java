bcb663074a00376b6890392d32adfe6f1f35e383
Gurung-Manish <manishgrg496@gmail.com>
2023-06-30 12:53:19 +0100

    chaning all the files because of class diagram change

--- src/main/java/com/handyman/rest_handyman/repository/ServiceProviderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.handyman.rest_handyman.repository;
-
-import com.handyman.rest_handyman.model.ServiceProvider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceProviderRepository extends JpaRepository<ServiceProvider, String> {
-}
